0cdcf4cd4a5469bd61a317db21ee2e5ad9a64b77
Wes Williams <wes.williamz@gmail.com>
2013-12-13 13:22:31 +0800

    starting point

--- test/CellTest.js
+++ test/CellTest.js
@@ -1,11 +1,9 @@
 var assert = require("assert");
 var cell = require("cell.js").cell;
 
-describe('cell', function() {
-    describe('state', function(){
-        it('should return be dead by default', function() {
-            var deadCell = cell();
-            assert.equal('dead', deadCell.state());
-        });
+describe('environment', function () {
+    it('should be working', function () {
+        var deadCell = cell();
+        assert.equal('dead', deadCell.state());
     });
 });
