9651265b49a65a0e89405da8ee80aa2185f111ad
Wes Williams <wes.williamz@gmail.com>
2014-01-23 18:30:13 +0800

    fixed jshint

--- cell.js
+++ cell.js
@@ -1,19 +1,22 @@
-"use strict";
+(function() {
+  "use strict";
+
 var us = require('underscore');
 module.exports.cell = function(state, posX, posY) {
   return {
     state: function() {
       return state;
-    }
-    , x: posX
-    , y: posY
+    },
+    x: posX,
+    y: posY
   };
 };
 
 module.exports.world = function(cells) {
   return {
     live_cells: function() {
-      return us(cells).filter(function(cell) { return cell.state(); }
+      return us(cells).filter(function(cell) { return cell.state(); });
     }
   };
 };
+})();
