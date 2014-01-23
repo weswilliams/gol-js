c6ca08a31254720a535f8e387d446de31bfc001c
Wes Williams <wes.williamz@gmail.com>
2014-01-23 17:32:16 +0800

    setting up shouldjs

--- test/CellTest.js
+++ test/CellTest.js
@@ -1,9 +1,11 @@
+"use strict";
 var assert = require("assert");
+var should = require('should');
 var cell = require("cell.js").cell;
 
 describe('environment', function () {
-    it('should be working', function () {
-        var deadCell = cell();
-        assert.equal('dead', deadCell.state());
-    });
+  it('should be working', function () {
+    var deadCell = cell('dead');
+    deadCell.state().should.equal('dead');
+  });
 });
