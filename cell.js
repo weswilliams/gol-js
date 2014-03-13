0d6865dd9fdaf766b4452e985a680c4965caf510
weswilliams <wes.williamz@gmail.com>
2014-03-13 17:17:56 +0800

    rules impl

--- cell.js
+++ cell.js
@@ -7,18 +7,6 @@ var cell = function(state, posX, posY) {
     state: function() {
       return state;
     },
-    nextLife: function(neighbours) {
-      var nextState = false;
-      var numberLiveCells = neighbours.liveNeighbors();
-      if (this.state() && numberLiveCells == 2) {
-        nextState = true;
-      }
-      if (numberLiveCells == 3)
-      {
-        nextState = true;
-      }
-      return cell(nextState, this.x, this.y);
-    },
     x: posX,
     y: posY
   };
--- test/CellTest.js
+++ test/CellTest.js
@@ -20,14 +20,6 @@ describe('cell', function () {
     liveCell = cell(true, 0, 1);
   });
 
-  var liveNeighbours = function(numberOfCells) {
-    return {
-      liveNeighbors: function () {
-        return numberOfCells;
-      }
-    };
-  };
-
   it('should have a state', function () {
     deadCell.state().should.equal(false);
   });
@@ -35,30 +27,5 @@ describe('cell', function () {
   it('should have coordinates', function () {
     deadCell.x.should.equal(0);
     deadCell.y.should.equal(1);
-  });
-
-  it('Any live cell with fewer than two live neighbours dies, as if caused by under-population.', function(){
-    liveCell.nextLife(liveNeighbours(0)).state().should.equal(false);
-    liveCell.nextLife(liveNeighbours(1)).state().should.equal(false);
-  });
-
-  it('Any live cell with two live neighbours lives on to the next generation', function() {
-    liveCell.nextLife(liveNeighbours(2)).state().should.equal(true);
-  });
-
-  it('Any live cell with three live neighbours lives on to the next generation', function() {
-    liveCell.nextLife(liveNeighbours(3)).state().should.equal(true);
-  });
-
-  it('Any live cell with more than three live neighbours dies, as if by overcrowding.', function() {
-    liveCell.nextLife(liveNeighbours(4)).state().should.equal(false);
-  });
-
-  it('Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.', function() {
-    deadCell.nextLife(liveNeighbours(3)).state().should.equal(true);
-  });
-
-  it('Any dead cell with exactly two live neighbours stays dead', function() {
-    deadCell.nextLife(liveNeighbours(2)).state().should.equal(false);
   });
 });
