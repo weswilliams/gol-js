f3e6340fd0d688e847c6741cccdd076bb0f767fa
Wes Williams <wes.williamz@gmail.com>
2014-04-06 09:52:36 +0800

    moving neighbor functionality into cell

--- cell.js
+++ cell.js
@@ -1,14 +1,47 @@
 (function () {
   "use strict";
 
+  var us = require("underscore");
+
   function coordinates(posX, posY) {
     return {
       x: posX,
       y: posY,
       hasSameLocationAs: function (compareCoordinates) {
         return this.x === compareCoordinates.x && this.y === compareCoordinates.y;
+      },
+      neighbors: function(coordinates) {
+        var me = this;
+        var neighborXRange = us.range(me.x - 1, me.x + 2);
+        var neighborYRange = us.range(me.y - 1, me.y + 2);
+        function and(predicates, parameters) {
+          return us.reduce(predicates, function(decision, predicate) {
+            return decision && predicate(parameters);
+          }, true);
+        }
+        function isXNeighbor(possibleNeighbor) {
+          return us.contains(neighborXRange, possibleNeighbor.x);
+        }
+        function isYNeighbor(possibleNeighbor) {
+          return us.contains(neighborYRange, possibleNeighbor.y);
+        }
+        function isNotMe(possibleNeighbor) {
+          return !me.hasSameLocationAs(possibleNeighbor);
+        }
+        function isNeighbor(possibleNeighbor) {
+          return and([isNotMe, isXNeighbor, isYNeighbor], possibleNeighbor);
+        }
+        var neighbors = us.filter(coordinates, isNeighbor);
+
+        return {
+          numberAlive: function() {
+            return us.filter(neighbors, function(neighbor) {
+              return neighbor.state();
+            }).length;
+          }
+        };
       }
-    }  ;
+    };
   }
 
   function aliveCell(coordinates) {
@@ -19,7 +52,8 @@
       },
       x: coordinates.x,
       y: coordinates.y,
-      hasSameLocationAs: coordinates.hasSameLocationAs
+      hasSameLocationAs: coordinates.hasSameLocationAs,
+      neighbors: coordinates.neighbors
     };
   }
 
@@ -31,13 +65,14 @@
       },
       x: coordinates.x,
       y: coordinates.y,
-      hasSameLocationAs: coordinates.hasSameLocationAs
+      hasSameLocationAs: coordinates.hasSameLocationAs,
+      neighbors: coordinates.neighbors
     };
   }
 
   module.exports.cell = function (state, posX, posY) {
     var myCoordinates = coordinates(posX, posY);
-    if (state) return aliveCell(myCoordinates);
+    if (state) { return aliveCell(myCoordinates); }
     return deadCell(myCoordinates);
   };
 })();
