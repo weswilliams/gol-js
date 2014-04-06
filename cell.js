7a5752363a88c3805e3bcf91f5db9ad48cff227c
Wes Williams <wes.williamz@gmail.com>
2014-04-06 16:27:54 +0800

    extracting function

--- cell.js
+++ cell.js
@@ -3,6 +3,28 @@
 
   var us = require("underscore");
 
+  function neighborsFilterFor(me) {
+    var neighborXRange = us.range(me.x - 1, me.x + 2);
+    var neighborYRange = us.range(me.y - 1, me.y + 2);
+    function and(predicates, parameters) {
+      return us.reduce(predicates, function(decision, predicate) {
+        return decision && predicate(parameters);
+      }, true);
+    }
+    function isXNeighbor(possibleNeighbor) {
+      return us.contains(neighborXRange, possibleNeighbor.x);
+    }
+    function isYNeighbor(possibleNeighbor) {
+      return us.contains(neighborYRange, possibleNeighbor.y);
+    }
+    function isNotMe(possibleNeighbor) {
+      return !me.hasSameLocationAs(possibleNeighbor);
+    }
+    return function isNeighbor(possibleNeighbor) {
+      return and([isNotMe, isXNeighbor, isYNeighbor], possibleNeighbor);
+    };
+  }
+
   function coordinates(posX, posY, cell) {
     return {
       x: posX,
@@ -18,27 +40,7 @@
         return this.x === compareCoordinates.x && this.y === compareCoordinates.y;
       },
       neighbors: function(possibleNeighbors) {
-        var me = this;
-        var neighborXRange = us.range(me.x - 1, me.x + 2);
-        var neighborYRange = us.range(me.y - 1, me.y + 2);
-        function and(predicates, parameters) {
-          return us.reduce(predicates, function(decision, predicate) {
-            return decision && predicate(parameters);
-          }, true);
-        }
-        function isXNeighbor(possibleNeighbor) {
-          return us.contains(neighborXRange, possibleNeighbor.x);
-        }
-        function isYNeighbor(possibleNeighbor) {
-          return us.contains(neighborYRange, possibleNeighbor.y);
-        }
-        function isNotMe(possibleNeighbor) {
-          return !me.hasSameLocationAs(possibleNeighbor);
-        }
-        function isNeighbor(possibleNeighbor) {
-          return and([isNotMe, isXNeighbor, isYNeighbor], possibleNeighbor);
-        }
-        var neighbors = us.filter(possibleNeighbors, isNeighbor);
+        var neighbors = us.filter(possibleNeighbors, neighborsFilterFor(this));
 
         return {
           numberAlive: function() {
