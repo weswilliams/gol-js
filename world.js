549f64295c94a0b76352d8479a80ac98e4dc711e
Wes Williams <wes.williamz@gmail.com>
2014-04-06 17:58:19 +0800

    renaming

--- world.js
+++ world.js
@@ -7,7 +7,7 @@
   var deadCell = cellModule.deadCell;
   var coordinates = cellModule.coordinates;
 
-  function createCell(hasALiveCell, x, y) {
+  function createCoordinates(hasALiveCell, x, y) {
     var cell = hasALiveCell ? liveCell() : deadCell();
     return coordinates(x, y, cell);
   }
@@ -15,7 +15,7 @@
   function parseRowPattern(rowPattern, rowIndex) {
     var rowCellStates = rowPattern.split('');
     return us.map(rowCellStates, function (cellState, columnIndex) {
-      return createCell(cellState === "1", columnIndex, rowIndex);
+      return createCoordinates(cellState === "1", columnIndex, rowIndex);
     });
   }
 
@@ -26,50 +26,63 @@
 
   module.exports = function (boardPattern) {
     var board = parseBoardPattern(boardPattern);
+
     function find(x, y) {
       return us.find(board, function (cell) {
         return cell.x === x && cell.y === y;
-      }) || createCell(false, x, y);
+      }) || createCoordinates(false, x, y);
     }
+
     function highest(dimension) {
-      return us.reduce(board, function(highestY, compareY) {
-        if (!highestY) { return compareY; }
-        if (highestY[dimension] > compareY[dimension]) { return highestY; }
-        return compareY;
+      return us.reduce(board, function (highest, compareTo) {
+        if (!highest) {
+          return compareTo;
+        }
+        if (highest[dimension] > compareTo[dimension]) {
+          return highest;
+        }
+        return compareTo;
       })[dimension] + 2;
     }
+
     function lowest(dimension) {
-      return us.reduce(board, function(highestY, compareY) {
-        if (!highestY) { return compareY; }
-        if (highestY[dimension] < compareY[dimension]) { return highestY; }
-        return compareY;
+      return us.reduce(board, function (lowest, compareTo) {
+        if (!lowest) {
+          return compareTo;
+        }
+        if (lowest[dimension] < compareTo[dimension]) {
+          return lowest;
+        }
+        return compareTo;
       })[dimension] - 1;
     }
 
     return {
-      nextLife: function() {
+      nextLife: function () {
         var nextBoard = [];
-        us.each(us.range(lowest('y'), highest('y')), function(yIndex) {
-          us.each(us.range(lowest('x'), highest('x')), function(xIndex) {
-            find(xIndex, yIndex).nextLife(board, function(livesInNextLife) {
-              if (livesInNextLife) { nextBoard.push(coordinates(xIndex, yIndex, liveCell())); }
+        us.each(us.range(lowest('y'), highest('y')), function (yIndex) {
+          us.each(us.range(lowest('x'), highest('x')), function (xIndex) {
+            find(xIndex, yIndex).nextLife(board, function (livesInNextLife) {
+              if (livesInNextLife) {
+                nextBoard.push(createCoordinates(livesInNextLife, xIndex, yIndex));
+              }
             });
           });
         });
-       board = nextBoard;
+        board = nextBoard;
       },
-      patternFor: function(startCell, endCell, alive, dead) {
+      patternFor: function (startCell, endCell, alive, dead) {
         alive = alive || "1";
         dead = dead || "0";
         var xRange = us.range(startCell.x, endCell.x + 1);
         var yRange = us.range(startCell.y, endCell.y + 1);
-        var pattern = us.reduce(yRange, function(pattern, yIndex) {
-          return us.reduce(xRange, function(pattern, xIndex) {
+        var pattern = us.reduce(yRange, function (pattern, yIndex) {
+          return us.reduce(xRange, function (pattern, xIndex) {
             var coordinates = find(xIndex, yIndex);
-            coordinates.cell.isAlive(function(isAlive) {
+            coordinates.cell.isAlive(function (isAlive) {
               pattern += isAlive ? alive : dead;
             });
-            return pattern ;
+            return pattern;
           }, pattern) + "\n";
         }, "");
         return pattern.substr(0, pattern.length - 1);
