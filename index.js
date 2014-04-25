5b5e52917923bb49fc9a8e186727f60518954770
weswilliams <wes.williamz@gmail.com>
2014-04-25 13:52:50 +0800

    adding an index.js to expose as a better npm module

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+(function() {
+  var gameOfLife = {};
+  var cellModule = require("./cell.js");
+  gameOfLife.liveCell = cellModule.liveCell;
+  gameOfLife.deadCell = cellModule.deadCell;
+  gameOfLife.parser = require('../../patternparser.js');
+  gameOfLife.world = require('../../world.js');
+  gameOfLife.pattern = function(patternName) {
+    return require('./patterName');
+  };
+  module.exports = gameOfLife;
+})();
